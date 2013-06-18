863ecaba32665c5f6431b4b259bdb4500fe335ae
MakerSquare Student <student@lab-3.att.net>
2013-06-18 16:35:13 -0500

    Added students scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130618205255) do
+ActiveRecord::Schema.define(:version => 20130618211851) do
 
   create_table "ideas", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(:version => 20130618205255) do
     t.string   "picture"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "students", :force => true do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "email"
+    t.text     "notes"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
